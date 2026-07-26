e68cc148fda7420f8f68b2c86b1d03b34f8e7f2a
Renz-kye B. Amoroto <amorotorenzkye8@gmail.com>
2026-07-26 20:03:25 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,98 @@
+// ===============================
+// MARKETGO PRODUCTS
+// ===============================
+
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "Microphone",
+    price: 450,
+    category: "Musical Instruments",
+    condition: "Second Hand",
+
+    seller: "Janjan Abonales",
+    sellerLink: "https://www.facebook.com/janjan.abonaled",
+    location: "Cabitan, Mandaon, Masbate",
+    date: "July 25, 2026 • 3:25 PM",
+
+    description: "Selling a second-hand microphone in good working condition. Complete and ready to use. Reason for selling: upgraded to a new microphone.",
+
+    images: [
+      "mic.jpg",
+      "mic2.jpg",
+      "mic3.jpg"
+    ],
+
+    featured: false,
+    latest: true,
+    favorite: false
+  },
+
+  {
+    id: 2,
+    name: "Transistor Radio",
+    price: 320,
+    category: "Musical Instruments",
+    condition: "Brand New",
+
+    seller: "LCC Placer",
+    sellerLink: "https://www.facebook.com/profile.php?id=100086186304359",
+    location: "Placer, Masbate",
+    date: "July 26, 2026 • 5:15 PM",
+
+    description: "Brand new transistor radio. Good quality, clear sound, and ready to use.",
+
+    images: [
+      "https://files.catbox.moe/bvydeh.jpg",
+      "https://files.catbox.moe/lkqxie.jpg",
+      "https://files.catbox.moe/2m1l8a.jpg"
+    ],
+
+    featured: false,
+    latest: true,
+    favorite: false
+  },
+  
+  {
+    id: 3,
+    name: "Flash Drive",
+    price: 250,
+    category: "Computers & Laptops",
+    condition: "Second Hand",
+
+    seller: "Junmar Ehurango",
+    sellerLink: "https://www.facebook.com/jun.mar.ehurango.2024",
+    location: "Centro, Placer, Masbate",
+    date: "July 26, 2026 • 5:15 PM",
+
+    description: "Selling a second-hand flash drive in good working condition. Fully functional and ready to use.",
+
+    images: [
+        "https://files.catbox.moe/w7xnyj.jpg",
+        "https://files.catbox.moe/nawo64.jpg"
+    ],
+
+    featured: false,
+    latest: true,
+    favorite: false
+}
+];
+// ===============================
+// MARKETGO CATEGORIES
+// ===============================
+
+const CATEGORIES = [
+  "Cellphones & Tablets",
+  "Computers & Laptops",
+  "School Supplies",
+  "Home Appliances",
+  "Furniture",
+  "Fashion",
+  "Shoes",
+  "Bags",
+  "Watches & Accessories",
+  "Cars & Motorcycles",
+  "Garden & Tools",
+  "Musical Instruments",
+  "Others"
+];
